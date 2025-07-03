a9dad8f4904ce916a96843cd1673b646936f0040
248416@st.takushoku-u.ac.jp <G484162024>
2025-07-03 16:19:56 +0900

    第五回課題

--- practice-js/hello.js
+++ practice-js/hello.js
@@ -1,5 +1,34 @@
-console.log('こんにちは');
-let x;
-x = 3;
-x = x+1;
-console.log(x);
+function printDom(data) {
+    
+    const oldResult = document.querySelector('div#result');
+    if (oldResult) {
+      oldResult.remove();
+    }
+  
+    
+    const resultDiv = document.createElement('div');
+    resultDiv.id = 'result';
+    document.body.appendChild(resultDiv);
+  
+    
+    const shops = data.results.shop;
+  
+    for (let shop of shops) {
+      const nameElem = document.createElement('h2');
+      nameElem.textContent = shop.name;
+  
+      const addressElem = document.createElement('p');
+      addressElem.textContent = `住所：${shop.address}`;
+  
+      const imgElem = document.createElement('img');
+      imgElem.src = shop.photo.pc.l;
+      imgElem.alt = shop.name;
+      imgElem.width = 400;
+  
+      
+      resultDiv.appendChild(nameElem);
+      resultDiv.appendChild(addressElem);
+      resultDiv.appendChild(imgElem);
+    }
+  }
+  
